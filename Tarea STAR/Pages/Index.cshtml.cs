0051f8aa90fa11a89cfb9e484fa5e734055b699e
Ismael Illanes <iiv94@hotmail.com>
2023-05-08 17:54:57 +0200

    Functions added

--- Tarea STAR/Pages/Index.cshtml.cs
+++ Tarea STAR/Pages/Index.cshtml.cs
@@ -9,11 +9,13 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private readonly AppDBContext _dbContext;
     public IEnumerable<Ventas> Ventas { get; set; }
-    [BindProperty(Name = "fechaInicio")]
-    public string FechaInicio { get; set; }
-    [BindProperty(Name = "fechaFinal")]
-    public string FechaFinal { get; set; }
+    [BindProperty]
+    public string DiaInicio { get; set; }
+    [BindProperty]
+    public string DiaFinal { get; set; }
+    [BindProperty]
     public string HoraInicio { get; set; }
+    [BindProperty]
     public string HoraFinal { get; set; }
     public IndexModel(ILogger<IndexModel> logger, AppDBContext dbContext)
     {
@@ -22,31 +24,42 @@ public class IndexModel : PageModel
         _dbContext = dbContext;
     }
 
-
     public void OnGet()
     {
 
     }
 
+    //public DateTime formateo(DateTime x)
+    //{
+        
+    //    var y = x.ToString();
+    //    var partes = y.Split(' ')[0];
+    //    DateTime fecha = DateTime.ParseExact(partes, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+    //    return fecha;
+    //}
+
+
     public IActionResult OnPost()
     {
-        string formato = "dd/MM/yyyy - HH:mm";
-        DateTime fechaHoraObj1 = DateTime.ParseExact(FechaInicio, formato, CultureInfo.InvariantCulture);
-        DateTime fechaHoraObj2 = DateTime.ParseExact(FechaFinal, formato, CultureInfo.InvariantCulture);
-        string fecha1 = fechaHoraObj1.ToString("dd/MM/yyyy");
-        string fecha2 = fechaHoraObj2.ToString("dd/MM/yyyy");
-        string hora1 = fechaHoraObj1.ToString("HH:mm");      
-        string hora2 = fechaHoraObj2.ToString("HH:mm");
+        DateTime fechaInicial = DateTime.ParseExact(DiaInicio, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        string fechaFormateada1 = fechaInicial.ToString("dd/MM/yyyy");
+        DateTime diaInicio = DateTime.ParseExact(fechaFormateada1, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+        DateTime fechaFinal = DateTime.ParseExact(DiaFinal, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        string fechaFormateada2 = fechaFinal.ToString("dd/MM/yyyy");
+        DateTime diaFinal = DateTime.ParseExact(fechaFormateada2, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+        TimeSpan horaInicio = TimeSpan.ParseExact(HoraInicio, "hh\\:mm", CultureInfo.InvariantCulture);
+        TimeSpan horaFinal = TimeSpan.ParseExact(HoraFinal, "hh\\:mm", CultureInfo.InvariantCulture);
 
 
+        
 
+        Ventas = _dbContext.Ventas
+            .Where(v => v.Fecha >= diaInicio && v.Fecha <= diaFinal && v.Hora >= horaInicio && v.Hora <= horaFinal)
+        .OrderBy(v => v.Fecha)
+        .ThenBy(v => v.Hora);
 
-        FechaInicio = fecha1;
-        HoraInicio = hora1;
-        FechaFinal = fecha2;
-        HoraFinal = hora2;
-
-        Ventas = _dbContext.Ventas.Where(v => v.Fecha >= fechaHoraObj1 && v.Fecha <= fechaHoraObj2);
 
         return Page();
     }
--- Tarea STAR/wwwroot/js/site.js
+++ Tarea STAR/wwwroot/js/site.js
@@ -1,24 +1,46 @@
 ﻿// OBTENCIÓN DE ELEMENTOS BODY
 
-//const checkBoxes = document.querySelectorAll('.checker');
+const checkBoxes = document.querySelectorAll('.checker');
+const initDay = document.querySelector('#initDay');
+const finalDay = document.querySelector('#finalDay');
+const initHour = document.querySelector('#initHour');
+const finalHour = document.querySelector('#finalHour');
+
+function checkAllCheckBoxes() {
+    if (checkBoxes[0].checked) {
+        for (let i = 1; i < checkBoxes.length; i++) {
+            checkBoxes[i].checked = true
+        }
+    } else {
+        for (let i = 1; i < checkBoxes.length; i++) {
+            checkBoxes[i].checked = false
+        }
+    }
+};
+
+initDay.addEventListener('click', function () {
+    finalDay.value = '';
+})
 
 
-//function checkAllCheckBoxes() {
-//    if (checkBoxes[0].checked) {
-//        for (let i = 1; i < checkBoxes.length; i++) {
-//            checkBoxes[i].checked = true
-//        }
-//    } else {
-//        for (let i = 1; i < checkBoxes.length; i++) {
-//            checkBoxes[i].checked = false
-//        }
-//    }
-//}
+initDay.addEventListener('input', function () {
+    if (initDay.value.length === 0) {
+        finalDay.disabled = true;
+    } else {
+        finalDay.disabled = false;
+    }
+})
+
+initHour.addEventListener('input', function () {
+    finalHour.min = initHour.value;
+})
+
+finalDay.addEventListener('click', function () {
+    finalDay.min = initDay.value;
+});
 
 
-
-//checkBoxes[0].addEventListener('click', checkAllCheckBoxes)
-
+checkBoxes[0].addEventListener('click', checkAllCheckBoxes)
 
 
 
@@ -26,31 +48,31 @@
 
 // CREACION DE CHART***********************************************************************************************************************************************************
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-        labels: ['col1', 'col2', 'col3'],
-        datasets: [{
-            label: ['Datos'],
-            data: [10, 9, 15],
-            backgroundColor: [
-                'rgb(66, 134, 244,0.5)',
-                'rgb(74, 135, 72,0.5)',
-                'rgb(229, 89, 50,0.5)'
-            ]
-        }]
-    },
-    options: {
-        responsive: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+//var ctx = document.querySelector(".Chart").getContext("2d");
+//var myChart = new Chart(ctx, {
+//    type: "bar",
+//    data: {
+//        labels: ['@item.Fecha'],
+//        datasets: [{
+//            label: ['Datos'],
+//            data: [10, 9, 15],
+//            backgroundColor: [
+//                'rgb(66, 134, 244,0.5)',
+//                'rgb(74, 135, 72,0.5)',
+//                'rgb(229, 89, 50,0.5)'
+//            ]
+//        }]
+//    },
+//    options: {
+//        responsive: false,
+//        scales: {
+//            yAxes: [{
+//                ticks: {
+//                    beginAtZero: true
+//                }
+//            }]
+//        }
+//    }
+//});
 
 
